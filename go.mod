7f4637d2a02dcaab38d2e45a9f1ca2f3c79cf578
Thomas Vendetta <thomas.vendetta@leaflink.com>
2024-06-28 11:37:38 -0400

    feat: added ability to move resources across states

---
+++ go.mod
@@ -0,0 +1,26 @@
+module tfmv
+
+go 1.21.0
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221212164502-fae10dda9338 // indirect
+	golang.org/x/mod v0.18.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/telemetry v0.0.0-20240607193123-221703e18637 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/tools v0.22.1-0.20240620150659-cb3016b76f3e // indirect
+	golang.org/x/tools/gopls v0.16.0 // indirect
+	golang.org/x/vuln v1.0.4 // indirect
+	honnef.co/go/tools v0.4.7 // indirect
+	mvdan.cc/gofumpt v0.6.0 // indirect
+	mvdan.cc/xurls/v2 v2.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var (
+	yellow  func(a ...interface{}) string
+	blue    func(a ...interface{}) string
+	magenta func(a ...interface{}) string
+	green   func(a ...interface{}) string
+
+	targetDir string
+	sourceDir string
+	tempDir   string
+	dryRun    bool
+)
+
+func init() {
+	yellow = color.New(color.FgYellow).SprintFunc()
+	blue = color.New(color.FgBlue).SprintFunc()
+	magenta = color.New(color.FgMagenta).SprintFunc()
+	green = color.New(color.FgGreen).SprintFunc()
+}
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "move-resource [terraform resource address]",
+		Short: "Move a terraform resource from source directory to target directory",
+		Args:  cobra.ExactArgs(1),
+		Run:   runTerraformMove,
+	}
+
+	rootCmd.Flags().StringVar(&targetDir, "target-dir", "", "Target directory to move a terraform resource to")
+	rootCmd.Flags().StringVar(&sourceDir, "source-dir", "", "Source directory to move a terraform resource from (optional, defaults to CWD)")
+	rootCmd.Flags().BoolVar(&dryRun, "dry-run", true, "If true, do not actually move the resource")
+
+	rootCmd.MarkFlagRequired("target-dir")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+const SourceStateFile string = "terraform.tfstate"
+const TargetStateFile string = "target-state.tfstate"
+
+func resolvePath(path string) string {
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return ""
+	}
+
+	if strings.HasPrefix(path, "~/") {
+		path = filepath.Join(usr.HomeDir, path[2:])
+	}
+
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return ""
+	}
+
+	return absPath
+}
+
+func runTerraformMove(cmd *cobra.Command, args []string) {
+	sourceDir = resolvePath(sourceDir)
+	targetDir = resolvePath(targetDir)
+
+	resourceAddress := args[0]
+	if sourceDir == "" {
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println("Error getting current working directory:", err)
+			os.Exit(1)
+		}
+		sourceDir = cwd
+	}
+
+	fmt.Printf(
+		"Moving resource %s: \n  From state %s \n  To state   %s \n",
+		yellow(resourceAddress),
+		blue(sourceDir),
+		blue(targetDir),
+	)
+
+	if dryRun {
+		fmt.Printf("%s enabled. Not actually making moves. \n", magenta("[dry-run]"))
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Do you want to continue? (yes/no): ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if !strings.EqualFold(input, "yes") {
+			fmt.Println("Exiting...")
+			os.Exit(1)
+		}
+	}
+
+	newTempDir, err := ioutil.TempDir("", "terraform-move")
+	if err != nil {
+		fmt.Println("Error creating temporary directory:", err)
+		os.Exit(1)
+	}
+	tempDir = newTempDir
+	defer os.RemoveAll(tempDir)
+
+	sourceStatePath := filepath.Join(tempDir, SourceStateFile)
+	targetStatePath := filepath.Join(tempDir, TargetStateFile)
+
+	if err := pullState(sourceDir, sourceStatePath); err != nil {
+		fmt.Println("Error pulling state from source directory:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("==> %s\n", green("Pulled source state"))
+
+	if err := pullState(targetDir, targetStatePath); err != nil {
+		fmt.Println("Error pulling state from target directory:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("==> %s\n", green("Pulled target state"))
+
+	if err := moveResource(tempDir, targetStatePath, resourceAddress); err != nil {
+		fmt.Println("Error moving resource:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("==> %s\n", green("Moved resource from source to target in local state"))
+
+	if err := copyFile(targetStatePath, filepath.Join(targetDir, TargetStateFile)); err != nil {
+		fmt.Println("Error copying target state file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("==> %s\n", green("Copied updated target state to target directory"))
+
+	if err := pushState(targetDir, TargetStateFile); err != nil {
+		fmt.Println("Error pushing state to target directory:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("==> %s\n", green("Pushed updated target state. Resource move success!"))
+
+	if !dryRun {
+		err = os.Remove(filepath.Join(targetDir, TargetStateFile))
+		if err != nil {
+			fmt.Println("Error cleaning up filepath:", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func pullState(dir, outPath string) error {
+	if dryRun {
+		prettyOutPath := strings.Replace(outPath, tempDir, "$temp", 1)
+
+		fmt.Printf(
+			"%s Would run 'terraform state pull': \n  from %s\n  write to %s\n",
+			magenta("[dry-run]"),
+			dir,
+			prettyOutPath,
+		)
+
+		return nil
+	} else {
+		cmd := exec.Command("terraform", "state", "pull")
+		cmd.Dir = dir
+		out, err := cmd.Output()
+		if err != nil {
+			return err
+		}
+		return ioutil.WriteFile(outPath, out, 0644)
+	}
+}
+
+func moveResource(tempDir, statePath, resourceAddress string) error {
+	if dryRun {
+		prettyStatePath := strings.Replace(statePath, tempDir, "$temp", 1)
+
+		fmt.Printf(
+			"%s Would run 'terraform state mv --state-out=%s'\n",
+			magenta("[dry-run]"),
+			prettyStatePath,
+		)
+
+		return nil
+	} else {
+		cmd := exec.Command("terraform", "state", "mv", "--state-out="+statePath, resourceAddress, resourceAddress)
+		cmd.Dir = tempDir
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		return cmd.Run()
+	}
+}
+
+func copyFile(src, dst string) error {
+	if dryRun {
+		prettySrcPath := strings.Replace(src, tempDir, "$temp", 1)
+
+		fmt.Printf(
+			"%s Would copy mutated state file over to target state directory\n  from %s\n  to %s\n",
+			magenta("[dry-run]"),
+			prettySrcPath,
+			dst,
+		)
+
+		return nil
+	} else {
+		input, err := ioutil.ReadFile(src)
+		if err != nil {
+			return err
+		}
+		return ioutil.WriteFile(dst, input, 0644)
+	}
+}
+
+func pushState(dir, stateFile string) error {
+	if dryRun {
+
+		fmt.Printf(
+			"%s Would run 'terraform state push %s' from %s\n",
+			magenta("[dry-run]"),
+			TargetStateFile,
+			dir,
+		)
+
+		return nil
+	} else {
+		cmd := exec.Command("terraform", "state", "push", stateFile)
+		cmd.Dir = dir
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		return cmd.Run()
+	}
+}
